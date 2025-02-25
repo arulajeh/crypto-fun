bd50d068a139e13b8159c1f83cfa7033c33c1ace
arulajeh <syahrulloh3@gmail.com>
2025-02-25 23:01:27 +0700

    set listen host

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,12 @@ use crate::handlers::price::*;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    println!("Listen at port 8080");
     HttpServer::new(|| {
         App::new()
             .route("/fetch", web::get().to(fetch_crypto_data))
     })
-        .bind("127.0.0.1:8080")?
+        .bind("0.0.0.0:8080")?
         .run()
         .await
 }
